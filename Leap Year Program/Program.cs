39386e0da25247d73f9a717af540f706c9e83e7e
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-14 08:48:19 +0530

    Leap Year

---
+++ Leap Year Program/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Leap_Year_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Please enter a year : ");
+            int Leapyear = Convert.ToInt32(Console.ReadLine());
+
+            if (Leapyear < 1000 || Leapyear > 9999)
+            {
+                Console.WriteLine("Invalid year. Please enter a 4-digit year.");
+            }
+            else if ((Leapyear % 4 == 0 && Leapyear % 100 != 0) || Leapyear % 400 == 0)
+            {
+                Console.WriteLine("{0} is a leap year.", Leapyear);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year.", Leapyear);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
