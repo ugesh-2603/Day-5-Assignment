83a059b52482906aca17672f52112999222bff89
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-14 07:45:52 +0530

    Flip the coin

---
+++ Day 5 Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Day_5_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of times to flip the coin:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int heads = 0, tails = 0;
+
+            Random Flip = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                if (Flip.NextDouble() < 0.5)
+                {
+                    tails++;
+                }
+                else
+                {
+                    heads++;
+                }
+            }
+
+            double headsPercentage = ((double)heads / n) * 100;
+            double tailsPercentage = ((double)tails / n) * 100;
+
+            Console.WriteLine("Heads: " + headsPercentage.ToString("0.00") + "%");
+            Console.WriteLine("Tails: " + tailsPercentage.ToString("0.00") + "%");
+        }
+    }
+}
