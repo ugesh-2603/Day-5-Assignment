1fba70717f3a4a38bc95d5648c51a9999d224492
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-15 09:50:33 +0530

    Alphabet is Vowel or Consonant

---
+++ Alphabet is Vowel or Consonant/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Alphabet_is_Vowel_or_Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an alphabet:");
+            char i = Convert.ToChar(Console.ReadLine().ToLower()); // convert to lowercase 
+
+            if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u')
+            {
+                Console.WriteLine("The alphabet is a vowel.");
+            }
+            else
+            {
+                Console.WriteLine("The alphabet is a consonant.");
+            }
+
+            Console.ReadLine(); // pause at the end of the program
+        }
+    }
+}
