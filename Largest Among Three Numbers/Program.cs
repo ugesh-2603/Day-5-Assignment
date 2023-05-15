19d41d0f80a739ef38d1386eb3a9766ab47ea2b6
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-15 09:58:30 +0530

    Largest Among Three Numbers

---
+++ Largest Among Three Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Largest_Among_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2, Number3;
+
+            Console.WriteLine("Enter the first number: ");
+            Number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            Number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number: ");
+            Number3 = int.Parse(Console.ReadLine());
+
+            if (Number1 > Number2 && Number1 > Number3)
+            {
+                Console.WriteLine("The largest number is: " + Number1);
+            }
+            else if (Number2 > Number1 && Number2 > Number3)
+            {
+                Console.WriteLine("The largest number is: " + Number2);
+            }
+            else
+            {
+                Console.WriteLine("The largest number is: " + Number3);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
