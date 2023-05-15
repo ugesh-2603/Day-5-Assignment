7f649bd4b6e53fcc502f6e0ea6ff0a1b7f336267
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-15 07:54:10 +0530

    Compute Quatient and Remainder

---
+++ Compute Quotient and Remainder/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Compute_Quotient_and_Remainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          //declare and initialize character variable
+
+           int a = 8, b = 2;
+           int q, r;
+
+          //find the quotient
+
+           q = a / b;
+
+          //find the remainder
+
+           r = a % b;
+
+          // display result
+
+           Console.WriteLine("When " + a + " is divided by " + b + ", quotient is " + q + " and remainder is " + r + ".");
+
+          // wait for user to press any key
+
+           Console.ReadKey();
+            
+        }
+    }
+}
+    
