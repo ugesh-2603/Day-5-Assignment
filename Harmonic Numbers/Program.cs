f9b4fcb50d071054ee5880692e2635a019286e60
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-15 07:35:54 +0530

    Harmonic Numbers

---
+++ Harmonic Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Harmonic_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n; // n is number of terms
+            double s = 0; // sum of series intialized with 0.
+
+            Console.Write("\n\n");
+            Console.WriteLine("Calculate the Harmonic Numbers:\n");
+            Console.Write("----------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.WriteLine("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            for (i = 1; i <= n; i++) //// it will print 1/i current value of the loop.
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i; // sum is calculated when converting i to s.
+            }
+            Console.WriteLine("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
+    }
+}
