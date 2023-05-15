4afe44ec088da9de4d30db81ffcc1f4c3d3f12b4
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-15 07:42:43 +0530

    Prime Factorization

---
+++ Prime Factorization/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Prime_Factorization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.WriteLine("Please Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factors:");
+            for (i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
